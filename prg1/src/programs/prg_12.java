cfd984b9255bc3833d8edd801c974cf9cb41daf8
Meghana6260 <addalamegha22@gmail.com>
2025-07-27 11:47:04 +0530

    first 13 programs.

---
+++ prg1/src/programs/prg_12.java
@@ -0,0 +1,12 @@
+package programs;
+import java.util.Scanner;
+public class prg_12 {
+	 public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 System.out.print("Enter a number: ");
+	 int a = sc.nextInt();
+	 int result = a << 1;
+	 System.out.println(a + " * 2 = " + result);
+	 sc.close();
+	 }
+}
