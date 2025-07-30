d635ff1f2333d227bd8e0faeb65579978d757823
Meghana6260 <addalamegha22@gmail.com>
2025-07-30 12:15:30 +0530

    .

---
+++ Exception_handling/src/exception/read_write.java
@@ -0,0 +1,27 @@
+package exception;
+import java.io.File;
+public class read_write {
+	       public static void main(String[] args) {
+	        // Create a File object
+	        File my_file_dir = new File("/home/students/abc.txt");
+	         if (my_file_dir.canWrite()) 
+	           {
+	            System.out.println(my_file_dir.getAbsolutePath() + " can write.\n");
+	           } 
+	         else
+	          {
+	            System.out.println(my_file_dir.getAbsolutePath() + " cannot write.\n");
+	          }
+	         if (my_file_dir.canRead()) 
+	           {
+	            System.out.println(my_file_dir.getAbsolutePath() + " can read.\n");
+	           } 
+	         else
+	          {
+	            System.out.println(my_file_dir.getAbsolutePath() + " cannot read.\n");
+	          }  
+	      }
+	  }
+	  
+
+
